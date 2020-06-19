b689e29108ea3f2b07301998f5db678ba471414d
Piotr Doan <doanpiotr@gmail.com>
2020-06-19 07:49:11 +0200

    Implement projectile collision and damaging logic

--- Source/Reikonoids/Actors/RProjectile.cpp
+++ Source/Reikonoids/Actors/RProjectile.cpp
@@ -1,29 +1,57 @@
 #include "RProjectile.h"
+#include <Components/SphereComponent.h>
 
 ARProjectile::ARProjectile()
 {
     PrimaryActorTick.bCanEverTick = true;
+
+    // Setup sphere collision component.
+    SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+    SphereCollision->SetSphereRadius(16.0f);
+}
+
+void ARProjectile::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    // React when projectile hits another actor.
+    AActor::OnActorHit.AddDynamic(this, &ARProjectile::OnActorHit);
 }
 
 void ARProjectile::BeginPlay()
 {
     Super::BeginPlay();
+
+    // Expire projectile when life time ends.
+    GetWorld()->GetTimerManager().SetTimer(LifetimeTimer, this, &ARProjectile::OnExpire, LifeTime, false);
+
+    // Orientate projectile towards direction.
+    SetActorRotation(Direction.ToOrientationRotator());
 }
 
 void ARProjectile::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    // Update life time.
-    LifeTime = std::max(0.0f, LifeTime - DeltaTime);
-    if(LifeTime == 0.0f)
+    // Move projectile forward at constant speed with sweep enabled.
+    SetActorLocation(GetActorLocation() + Direction * Speed * DeltaTime, true);
+}
+
+void ARProjectile::OnActorHit(AActor* Self, AActor* Other, FVector NormalImpulse, const FHitResult& Hit)
+{
+    // Make hit actor receive damage.
+    if(Other != nullptr)
     {
-        Destroy();
+        FDamageEvent DamageEvent;
+        Other->TakeDamage(Damage, DamageEvent, nullptr, this);
     }
 
-    // Orientate projectile towards direction.
-    SetActorRotation(Direction.ToOrientationRotator());
+    // Destroy projectile.
+    Destroy();
+}
 
-    // Move projectile forward at constant speed.
-    SetActorLocation(GetActorLocation() + Direction * Speed * DeltaTime);
+void ARProjectile::OnExpire()
+{
+    // Destroy when life time ends.
+    Destroy();
 }
--- Source/Reikonoids/Actors/RProjectile.h
+++ Source/Reikonoids/Actors/RProjectile.h
@@ -3,24 +3,42 @@
 #include <GameFramework/Actor.h>
 #include "RProjectile.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class REIKONOIDS_API ARProjectile : public AActor
 {
     GENERATED_BODY()
 
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly)
+    float Damage = 10.0f;
+
+    UPROPERTY(EditDefaultsOnly)
     float LifeTime = 3.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly)
     float Speed = 1000.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(BlueprintReadOnly)
     FVector Direction = FVector::ForwardVector;
 
 protected:
     ARProjectile();
 
+    virtual void PostInitializeComponents() override;
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
+
+    UFUNCTION()
+    void OnActorHit(AActor* Self, AActor* Other, FVector NormalImpulse, const FHitResult& Hit);
+
+    UFUNCTION()
+    void OnExpire();
+
+private:
+    FTimerHandle LifetimeTimer;
+
+    UPROPERTY(VisibleAnywhere)
+    USphereComponent* SphereCollision = nullptr;
 };
